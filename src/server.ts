3502a28a7b5ce3d95d9c96a04fec0aa0a840d017
Md. Arman Hossain <mahpro110@gmail.com>
2023-06-08 06:46:43 +0600

    lint staged setup done

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function boostrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`db connected successfully`);
+
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(`failed to connect db`, error);
+  }
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+boostrap();
